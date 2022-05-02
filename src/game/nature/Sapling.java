bb0f311525c323a7a8a628e3cb5db781424bc864
llow0009 <llow0009@student.monash.edu>
2022-05-02 10:39:01 +0800

    removing literals in nature package

--- src/game/nature/Sapling.java
+++ src/game/nature/Sapling.java
@@ -6,8 +6,15 @@ import game.currency.Coin;
 import java.util.Random;
 
 public class Sapling extends Tree implements Growable{
+    private double spawnCoinChance;
+    private int spawnCoinValue;
+    private int maturityAge;
     public Sapling(){
+
         super ('t', "Sapling", 80,20 );
+        this.spawnCoinChance = 0.1;
+        this.spawnCoinValue = 20;
+        this.maturityAge = 9;
     }
 
     @Override
@@ -20,14 +27,14 @@ public class Sapling extends Tree implements Growable{
         super.setAge(super.getAge() + 1);
 
         // grow every 10 turns
-        if (super.getAge() > 9){
+        if (super.getAge() > this.maturityAge){
             grow(location);
         }
 
 
         // has a 10% chance to produce/spawn a coin ($20) on its location at every turn.
-        if (new Random().nextDouble() <= 0.1){
-            location.addItem(new Coin(20));
+        if (new Random().nextDouble() <= this.spawnCoinChance){
+            location.addItem(new Coin(this.spawnCoinValue));
         }
     }
 }
