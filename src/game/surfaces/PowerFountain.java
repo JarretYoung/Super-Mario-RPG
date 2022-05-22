bd52b51fefab2e6785b690de542924dceb997e13
JASTEJ SINGH GILL, SAVINDER SINGH <jastej90210@gmail.com>
2022-05-22 23:42:45 +0800

    Updated documentation

--- src/game/surfaces/PowerFountain.java
+++ src/game/surfaces/PowerFountain.java
@@ -1,5 +1,10 @@
 package game.surfaces;
 
+/**
+ * Class for power fountain
+ * @author Jastej Gill
+ * @version 2.0 21/5/2022
+ */
 public class PowerFountain extends Fountain{
     /**
      * Constructor
